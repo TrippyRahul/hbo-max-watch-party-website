46bf68fa0d69df803c267b684b6456b487e0540c
TrippyGaurav <gaurav.trippybug@gmail.com>
2023-12-15 11:24:47 +0530

    update btn link

--- src/app/components/Button.jsx
+++ src/app/components/Button.jsx
@@ -4,7 +4,7 @@ import styles from "../styles/button.module.scss";
 const Button = () => {
   return (
     <a
-      href="https://chrome.google.com/webstore/detail/watchottparty/akbbdmilignoplkhminiaecccigmpdmo?hl=en-US"
+      href="https://chromewebstore.google.com/detail/hbo-watch-party/ojkhlfmofplghgodaihagbdhohmanmcb?hl=en&pli=1"
       target="_blank"
     >
       <button className={styles.btn}>Add to Chrome</button>
